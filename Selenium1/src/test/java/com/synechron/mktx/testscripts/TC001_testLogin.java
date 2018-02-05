3367e17b0e321c5767b4ea7db787b4d1a314e07a
pranshu-git <32287965+pranshu-git@users.noreply.github.com>
2018-02-05 15:31:54 +0530

    changes in test script
    changed @beforeclass to @beforetest in test scripts

--- Selenium1/src/test/java/com/synechron/mktx/testscripts/TC001_testLogin.java
+++ Selenium1/src/test/java/com/synechron/mktx/testscripts/TC001_testLogin.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.junit.BeforeClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.synechron.mktx.pagelibrary.Signin;
@@ -12,15 +13,18 @@ import com.synechron.mktx.testbase.TestBase;
 public class TC001_testLogin extends TestBase{
 	Signin signin;//Reference of signin class
 	
-	@BeforeClass
+	@BeforeTest
 	public void SETUP() throws IOException{
+		System.out.println("init");
 		init();
 	}
 	
 	@Test
 	public void testLogin(){
+		System.out.println("test");
 		signin = new Signin(driver);
 		signin.clickonSignIn();
+		signin.EntercreateAccEmailAdd("123@sd@.com");
 	}
 	
 	@AfterTest
